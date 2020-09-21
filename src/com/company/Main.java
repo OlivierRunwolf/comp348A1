11b7905d61cab54e0bd12d5d62b229b862870e5f
Olivier <Olivier@tractiondk.com>
2020-09-21 11:25:20 -0400

    reader

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+            List<Employee> listEmployees = new ArrayList<>();
+        try{
+            File myFile = new File("employeRecords.txt");
+            Scanner reader = new Scanner(myFile);
+            while(reader.hasNextLine()){
+                String data = reader.nextLine();
+                listEmployees.add(Employee.parse(data));
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
     }
 }
