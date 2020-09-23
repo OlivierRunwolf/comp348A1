1d53fab365cf77ec0f9862a0078947710c1cef0c
gkillick <gkillick@gmail.com>
2020-09-22 18:55:38 -0600

    missed the files :)

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -7,43 +7,43 @@ public class Employee implements Person{
     String id,firstName,lastName;
     BigDecimal salary;
     Level salaryLevel;
-
+    
     //optional enum for salary
     enum Level {
-        UNDER25 ("under $25,000.00"),
-        UNDER40 ("under $40,000.00"),
-        UNDER70 ("under $70,000.00"),
-        OVER70 ("over $70,000.00");
-        private String levelString;
-        private Level(String levelString) {
-            this.levelString = levelString;
-        }
-        @Override
-        public String toString() {
-            return this.levelString;
-        }
-
+    	UNDER25 ("under $25,000.00"),
+    	UNDER40 ("under $40,000.00"),
+    	UNDER70 ("under $70,000.00"),
+    	OVER70 ("over $70,000.00");
+    	private String levelString; 
+    	private Level(String levelString) {
+    		this.levelString = levelString;
+    	}
+    	@Override
+    	public String toString() {
+    		return this.levelString;
+    	}
+    	
     }
 
 
     public Employee(String id,String firstName,String lastName,BigDecimal salary){
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.salary = salary;
-
-        //assign salary level
-        int intSal = salary.intValue();
-        if(intSal < 25000) {
-            this.salaryLevel = Level.UNDER25;
-        }else if(intSal < 40000) {
-            this.salaryLevel = Level.UNDER40;
-        }else if (intSal < 70000) {
-            this.salaryLevel = Level.UNDER70;
-        }else {
-            this.salaryLevel = Level.OVER70;
-        }
-
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.salary = salary;
+    
+    //assign salary level
+    int intSal = salary.intValue();
+    if(intSal < 25000) {
+    	this.salaryLevel = Level.UNDER25;
+    }else if(intSal < 40000) {
+    	this.salaryLevel = Level.UNDER40;
+    }else if (intSal < 70000) {
+    	this.salaryLevel = Level.UNDER70;
+    }else {
+    	this.salaryLevel = Level.OVER70;
+    }
+  
     }
 
 
@@ -54,9 +54,9 @@ public class Employee implements Person{
     public BigDecimal getBigDecimal(){
         return salary;
     };
-
+    
     public double getSalary() {
-        return salary.doubleValue();
+    	return salary.doubleValue();
     }
     static public Employee parse(String input){
         String id,firstName,lastName;
@@ -69,12 +69,12 @@ public class Employee implements Person{
         salary = new BigDecimal(tempArray[3]);
         return new Employee(id,firstName,lastName,salary);
     }
-
+    
     //return level of salary
     public Level getLevel() {
-        return this.salaryLevel;
+    	return this.salaryLevel;
     }
-
+    
     @Override
     public String getPersonId() {
         return id;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,65 +20,65 @@ import com.company.Employee.Level;
 public class Main {
 
     public static void main(String[] args) {
-
-        Employee[] employees;
-        Stream<Employee> employeeStream;
-
-        //open file
+    	
+    	Employee[] employees;
+    	Stream<Employee> employeeStream;
+        
+    	//open file 
         try(Stream<String> fileStream = Files.lines(Paths.get("employeeRecords.txt"))){
-
-            //map lines to employee objects
-            employees = fileStream
-                    .map(x -> Employee.parse(x))
-                    .toArray(Employee[]::new);
-
-            fileStream.close();
-
-            System.out.println("Employees sorted by last name then first name:");
-            Arrays.sort(employees, (a, b) -> a.firstName.compareTo(b.firstName));
-            Arrays.sort(employees, (a, b) -> a.lastName.compareTo(b.lastName));
-            employeeStream = Arrays.stream(employees);
-            //display list
-            employeeStream.forEach(System.out::println);
-            employeeStream.close();
-
-            System.out.println();
-
-            System.out.println("Employees sorted by id:");
-            Arrays.sort(employees, (a,b) -> Integer.parseInt(a.id) - Integer.parseInt(b.id));
-            employeeStream = Arrays.stream(employees);
-
-            employeeStream.forEach(System.out::println);
-            employeeStream.close();
-
-            //summary information
-            System.out.println("\nEmployees Summary:");
-            employeeStream = Arrays.stream(employees);
-            Map<Level, DoubleSummaryStatistics> byLevel = employeeStream.collect(Collectors
-                    .groupingBy(Employee::getLevel, Collectors.summarizingDouble(Employee::getSalary)));
-
-            byLevel.entrySet().stream()
-                    .sorted((a,b) -> (int)a.getValue().getAverage() - (int)b.getValue().getAverage())
-                    .forEachOrdered((e) ->
-                            System.out.println("There are "+e.getValue().getCount()+ " employees making "+ e.getKey() + " with an average salary of $"+ String.format("%.2f",e.getValue().getAverage()))
-                    );
-
-            employeeStream.close();
-
-
-
-
-
+            
+        	//map lines to employee objects
+        	employees = fileStream
+        			.map(x -> Employee.parse(x))
+        			.toArray(Employee[]::new);
+        	
+        	fileStream.close();
+        	
+        	System.out.println("Employees sorted by last name then first name:");
+        	Arrays.sort(employees, (a, b) -> a.firstName.compareTo(b.firstName));
+        	Arrays.sort(employees, (a, b) -> a.lastName.compareTo(b.lastName));
+        	employeeStream = Arrays.stream(employees);
+        	//display list
+        	employeeStream.forEach(System.out::println);
+        	employeeStream.close();
+        	
+        	System.out.println();
+        	
+        	System.out.println("Employees sorted by id:");
+        	Arrays.sort(employees, (a,b) -> Integer.parseInt(a.id) - Integer.parseInt(b.id));
+        	employeeStream = Arrays.stream(employees);
+        	
+        	employeeStream.forEach(System.out::println);
+        	employeeStream.close();
+        	
+        	//summary information
+        	System.out.println("\nEmployees Summary:");
+        	employeeStream = Arrays.stream(employees);
+        	Map<Level, DoubleSummaryStatistics> byLevel = employeeStream.collect(Collectors
+        			.groupingBy(Employee::getLevel, Collectors.summarizingDouble(Employee::getSalary)));
+        	
+        	byLevel.entrySet().stream()
+        		.sorted((a,b) -> (int)a.getValue().getAverage() - (int)b.getValue().getAverage())
+        		.forEachOrdered((e) ->
+        			System.out.println("There are "+e.getValue().getCount()+ " employees making "+ e.getKey() + " with an average salary of $"+ String.format("%.2f",e.getValue().getAverage()))
+        			);
+        	
+        	employeeStream.close();
+        	
+        	
+        	
+            
+            
         }catch (IOException e){
             System.out.println("File not found");
-
+        
         }
+        
+        
 
-
-
-
-
-
-
+        
+        
+ 
+        
     }
 }
